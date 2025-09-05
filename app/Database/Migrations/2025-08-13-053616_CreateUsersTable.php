8618471b2cef71c6fd5cf54b7c29573590f45580
spiegel123 <enzopro2222@gmail.com>
2025-09-05 21:22:30 +0800

    updated user table

--- app/Database/Migrations/2025-08-13-053616_CreateUsersTable.php
+++ app/Database/Migrations/2025-08-13-053616_CreateUsersTable.php
@@ -13,7 +13,7 @@ class CreateUsersTable extends Migration
             'name'       => ['type' => 'VARCHAR', 'constraint' => 100],
             'email'      => ['type' => 'VARCHAR', 'constraint' => 100, 'unique' => true],
             'password'   => ['type' => 'VARCHAR', 'constraint' => 255],
-            'role'       => ['type' => 'ENUM', 'constraint' => ['student', 'instructor', 'admin'], 'default' => 'student'],
+            'role'       => ['type' => 'ENUM', 'constraint' => ['admin', 'user'], 'default' => 'user'],
             'created_at' => ['type' => 'DATETIME', 'null' => true],
             'updated_at' => ['type' => 'DATETIME', 'null' => true],
         ]);
