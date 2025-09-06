72b34c02c6d308b46ecea6bc8aad613c1401eb2e
spiegel123 <enzopro2222@gmail.com>
2025-09-06 09:58:38 +0800

    added login and register navigation for about and contact

--- app/Views/contact.php
+++ app/Views/contact.php
@@ -24,6 +24,12 @@
                     <li class="nav-item">
                         <a class="nav-link active" href="/contact">Contact</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/login">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/register">Register</a>
+                    </li>
                 </ul>
             </div>
         </div>
